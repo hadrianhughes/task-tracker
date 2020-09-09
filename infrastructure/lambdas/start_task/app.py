5511eaac24c4500369a6a674f9bfc41473d2d716
Hadrian Hughes <hadrian.hughes@and.digital>
2020-09-09 11:28:26 +0100

    Successfully adding task sessions to S3 file

--- infrastructure/lambdas/start_task/app.py
+++ infrastructure/lambdas/start_task/app.py
@@ -1,4 +1,5 @@
 import json
+import boto3
 
 responses = {
     'no_task_name': (400, 'A task_name must be included in the request body')
@@ -14,6 +15,24 @@ def responseFor(key):
             'body': key_response[1]
         }
 
+
+def add_task_session(task_name):
+    try:
+        s3 = boto3.resource('s3')
+        db = s3.Object(bucket_name='task-tracker-mvp-bucket', key='task-tracker-db.json')
+        dbResponse = db.get()
+        data = dbResponse['Body'].read()
+    except:
+        print('Something went wrong when accessing the S3 Bucket')
+
+    json_data = json.loads(data)
+    sessions = json_data['sessions']
+
+    if len(sessions) == 0:
+        sessions.append({ 'task_name': task_name })
+        db.put(Body=json.dumps({ 'sessions': sessions }))
+
+
 def lambda_handler(event, context):
     if event['body'] == None:
         return responseFor('no_task_name')
@@ -24,5 +43,7 @@ def lambda_handler(event, context):
         task_name = body['task_name']
     else:
         return responseFor('no_task_name')
+
+    add_task_session(task_name)
 
     return { 'statusCode': 200 }
