cf80dd0b7f638c5b6b5b3ae173a21ec13f4f2bf2
Hadrian Hughes <hadrian.hughes@and.digital>
2020-09-09 09:25:36 +0100

    Add guard statements to check for request body in start lambda

--- infrastructure/lambdas/start_task/app.py
+++ infrastructure/lambdas/start_task/app.py
@@ -1,2 +1,28 @@
+import json
+
+responses = {
+    'no_task_name': (400, 'A task_name must be included in the request body')
+}
+
+def responseFor(key):
+    if not key in responses:
+        raise KeyError('Response for ' + key + ' does not exist')
+    else:
+        key_response = responses[key]
+        return {
+            'statusCode': key_response[0],
+            'body': key_response[1]
+        }
+
 def lambda_handler(event, context):
-    return { "statusCode": 200 }
+    if event['body'] == None:
+        return responseFor('no_task_namez')
+
+    body = json.loads(event['body'])
+
+    if 'task_name' in body:
+        task_name = body['task_name']
+    else:
+        return responseFor('no_task_name')
+
+    return { 'statusCode': 200 }
